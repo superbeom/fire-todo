070b6e80a5f2f90b06acbea68cbd1d7e37e68846
superbeom <it.super.beom@gmail.com>
2020-07-12 15:06:32 +0900

    Establish a framework

---
+++ tempData.js
@@ -0,0 +1,66 @@
+// Temporary Data until we connect with Firebase
+
+export default tempData = [
+  {
+    name: "Plan a Trip",
+    color: "#24A6D9",
+    todos: [
+      {
+        title: "Book Flight",
+        completed: false,
+      },
+      {
+        title: "Passport Check",
+        completed: true,
+      },
+      {
+        title: "Reserve Hotel Room",
+        completed: true,
+      },
+      {
+        title: "Pack Luggage",
+        completed: false,
+      },
+    ],
+  },
+  {
+    name: "Errands",
+    color: "#8022D9",
+    todos: [
+      {
+        title: "Buy Milk",
+        completed: false,
+      },
+      {
+        title: "Make a Video",
+        completed: true,
+      },
+      {
+        title: "Take dog for a walk",
+        completed: true,
+      },
+      {
+        title: "Wash Dishes",
+        completed: true,
+      },
+    ],
+  },
+  {
+    name: "Birthday Party",
+    color: "#595BD9",
+    todos: [
+      {
+        title: "Get Balloons",
+        completed: false,
+      },
+      {
+        title: "Send Invitations",
+        completed: true,
+      },
+      {
+        title: "Make Dinner Reservations",
+        completed: true,
+      },
+    ],
+  },
+];
